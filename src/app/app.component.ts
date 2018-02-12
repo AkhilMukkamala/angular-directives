f292ea5b62b1ce6d8aeb082911775ebdcd9ef8af
Akhil Mukkamala <akhil.mukkamala@gmail.com>
2018-02-13 01:53:17 +0530

    ngIf Else Directives (ng-template) Explained

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Directives';
+
+  serverCreated: Boolean = false;
+  serverName: String = '';
+
+
+  
+  onCreateServer() {
+    this.serverCreated = true;
+  }
+
+
+
 }
